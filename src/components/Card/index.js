8e2fed58a352cdb168cc892882f921c6454fd0de
Beatrici <beatrici_ramos@hotmail.com>
2021-08-09 21:10:54 -0300

    fix

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,15 +3,15 @@ import { useDrag } from 'react-dnd'
 import { Container, Label } from './styles';
 
 export default function Card({ data }) {
-    // const [{ isDragging }, dragRef] = useDrag({
-    //     item: { type: 'CARD' },
-    //     collect: monitor => ({
-    //         isDragging: monitor.isDragging(),
-    //     })
-    // })
+    const [{ isDragging }, drag] = useDrag({
+        type: "CARD",
+        collect: (monitor) => ({
+            isDragging: monitor.isDragging(),
+        }),
+    });
 
     return (
-        <Container>
+        <Container ref={drag} >
             <header>
                 {data.labels.map(label => <Label key={label} color={label} />)}
             </header>
